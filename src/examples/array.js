b8dbbe0c56d51dc841fd3aed25d716a0d3b66f37
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-09-18 21:14:14 +0300

    updated

---
+++ src/examples/array.js
@@ -0,0 +1,14 @@
+export default {
+  data: [
+    "Gluten-Free",
+    "Peanut-Free",
+    "Seafood-Free",
+    "Sesame-Free",
+    "Soy-Free",
+    "Dairy-Free",
+    "Egg-Free",
+    "Sulfite-Free",
+    "Tree Nut-Free",
+    "Wheat-Free"
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import       thankYou from 'examples/modern';
+
+import data from 'examples/array';
+
+import groceries from 'examples/grocery';
+
+export const modern = thankYou;
+
+console.log(thankYou());
+
+console.log(groceries);
