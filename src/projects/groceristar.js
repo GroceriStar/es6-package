b1c09d6b1bd0705d0972806203ee693ec2d3fe3d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-09-20 02:31:40 +0300

    upd

--- src/projects/groceristar.js
+++ src/projects/groceristar.js
@@ -3,7 +3,7 @@ import departmentsData from 'data/department';
 import ingredientsData from 'data/ingredient';
 import usersData       from 'data/user';
 
-import _         from 'lodash';
+// import _         from 'lodash';
 // or use better way
 // import { map, uniq, tail } from 'lodash';
 
